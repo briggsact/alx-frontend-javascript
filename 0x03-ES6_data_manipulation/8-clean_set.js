9afd767591638baef548094a11064e2421655ad5
briggsact <99441221+briggsact@users.noreply.github.com>
2023-01-05 04:16:24 +0100

    my commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,21 +1,14 @@
-/*
-Create a function named cleanSet that returns a string of all the set values
-that start with a specific string (startString).
-It accepts two arguments: a set (Set) and a startString (String).
-When a value starts with startString you only append the rest of the string.
-The string contains all the values of the set separated by -.
-*/
-
+/* Create a function cleanSet
+   REturns startString and accepts two args */
 export default function cleanSet(set, startString) {
-  const startStringLen = startString.length;
-  const strArray = [];
-  if (startString && typeof startString === 'string') {
-    for (const str of set) {
-      if (str && str.startsWith(startString)) {
-        const splitstr = str.slice(startStringLen);
-        strArray.push(splitstr);
-      }
+  if (!startString || typeof startString !== 'string') return '';
+
+  const strList = [];
+  for (const str of set) {
+    if (typeof str === 'string' && str.startsWith(startString)) {
+      strList.push(str.slice(startString.length));
     }
   }
-  return strArray.join('-');
+
+  return strList.join('-');
 }
